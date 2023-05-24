134e76aa886285af09d47c51faf95727c3ec6f6c
Justin <96884681+justinpetri@users.noreply.github.com>
2023-05-23 22:46:40 -0400

    Update ICMP_sender.go

--- ICMP_sender.go
+++ ICMP_sender.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"reflect"
+	"time"
 
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 )
 
-const targetIP = "ENTER_TARGET_IP_HERE"
+const targetIP = "127.0.0.1"
 
 func main() {
 	// Creates listener for return packets
@@ -22,15 +23,13 @@ func main() {
 	defer c.Close()
 
 	// constructs packet
-	StringToEncode := "HELLO_CAN_YOU_READ_ME_NOAH"
-	Encoding := base64.StdEncoding.EncodeToString([]byte(StringToEncode))
-	fmt.Println("Sent Encoded string:", Encoding)
+	stringToEncode := "Hi"
 
 	wm := icmp.Message{
 		Type: ipv4.ICMPTypeEcho, Code: 0,
 		Body: &icmp.Echo{
 			ID: os.Getpid() & 0xffff, Seq: 1,
-			Data: []byte(Encoding),
+			Data: []byte(stringToEncode),
 		},
 	}
 	wb, err := wm.Marshal(nil)
@@ -38,28 +37,31 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// sends packet to targetIP, returns error message if error
-	if _, err := c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(targetIP)}); err != nil {
-		log.Fatalf("WriteTo err, %s", err)
+	// Converts string to binary
+	var binaryString string
+	for _, c := range stringToEncode {
+		binaryString = fmt.Sprintf("%s%.8b", binaryString, c)
 	}
 
-	// captures return message, returns error message if error
-	rb := make([]byte, 1500)
-	n, peer, err := c.ReadFrom(rb)
-	if err != nil {
-		log.Fatal(err)
+	fmt.Println(binaryString)
+	fmt.Println(reflect.TypeOf(binaryString))
+
+	// for _ in range binaryString -> if 0 then wait 2 seconds and second BUT if 1 then wait 1
+	for value := 0; value < len(binaryString); value++ {
+		fmt.Println(binaryString[value])
+		if binaryString[value] == 48 { // 48 is ASCII value of 0
+			time.Sleep(2 * time.Second)
+			fmt.Println("SENT 0")
+		} else {
+			time.Sleep(1 * time.Second)
+			fmt.Println("SENT 1")
+		}
+
+		// sends packet to targetIP
+		c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(targetIP)}) // NEED TO CONDENSE THIS
+
 	}
 
-	rm, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), rb[:n])
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Sent encoded string:", stringToEncode)
 
-	// display result
-	switch rm.Type {
-	case ipv4.ICMPTypeEchoReply:
-		log.Printf("got response from %v", peer)
-	default:
-		log.Printf("got %+v; want echo reply", rm)
-	}
 }
