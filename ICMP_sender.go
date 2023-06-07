2d8b677bbe1ab60a40bd2d1704a0b4f92a49ce72
Justin <96884681+justinpetri@users.noreply.github.com>
2023-06-06 22:55:27 -0400

    Update ICMP_sender.go

--- ICMP_sender.go
+++ ICMP_sender.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"reflect"
 	"time"
 
 	"golang.org/x/net/icmp"
@@ -15,53 +14,76 @@ import (
 const targetIP = "127.0.0.1"
 
 func main() {
-	// Creates listener for return packets
-	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
-	if err != nil { // if error, return listen error message
-		log.Fatalf("listen error, %s", err)
+
+	stringToEncode := "hi"
+	fmt.Println("\nSending:", stringToEncode)
+
+	binaryString := convertToBinary(stringToEncode)
+	sendICMPPackets(binaryString)
+
+}
+
+func createICMPPackets() (*icmp.PacketConn, []byte) {
+
+	icmpListener, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		log.Fatalf("ERROR with targetIP %s", err)
 	}
-	defer c.Close()
 
-	// constructs packet
-	stringToEncode := "Hi"
-
-	wm := icmp.Message{
-		Type: ipv4.ICMPTypeEcho, Code: 0,
+	startStopMessage := "message"
+	icmpPacket := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Code: 0,
 		Body: &icmp.Echo{
 			ID: os.Getpid() & 0xffff, Seq: 1,
-			Data: []byte(stringToEncode),
+			Data: []byte(startStopMessage),
 		},
 	}
-	wb, err := wm.Marshal(nil)
+
+	packetBinaryEncoding, err := icmpPacket.Marshal(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Converts string to binary
+	return icmpListener, packetBinaryEncoding
+
+}
+
+func convertToBinary(stringToEncode string) string {
+
 	var binaryString string
 	for _, c := range stringToEncode {
 		binaryString = fmt.Sprintf("%s%.8b", binaryString, c)
 	}
 
 	fmt.Println(binaryString)
-	fmt.Println(reflect.TypeOf(binaryString))
+	return binaryString
+}
 
-	// for _ in range binaryString -> if 0 then wait 2 seconds and second BUT if 1 then wait 1
+func sendICMPPackets(binaryString string) {
+	icmpListener, packetBinaryEncoding := createICMPPackets()
+
+	// START PACKET
+	icmpListener.WriteTo(packetBinaryEncoding, &net.IPAddr{IP: net.ParseIP(targetIP)})
+
 	for value := 0; value < len(binaryString); value++ {
-		fmt.Println(binaryString[value])
 		if binaryString[value] == 48 { // 48 is ASCII value of 0
-			time.Sleep(2 * time.Second)
-			fmt.Println("SENT 0")
+			time.Sleep(500 * time.Millisecond)
 		} else {
-			time.Sleep(1 * time.Second)
-			fmt.Println("SENT 1")
+			time.Sleep(1000 * time.Millisecond)
 		}
 
+		sentTime := time.Now().Format("15:04:05")
+		fmt.Printf("%s Sent %v\n", sentTime, string(binaryString[value]))
+
 		// sends packet to targetIP
-		c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(targetIP)}) // NEED TO CONDENSE THIS
+		icmpListener.WriteTo(make([]byte, 8), &net.IPAddr{IP: net.ParseIP(targetIP)}) // Allocates 8 bytes for "Echo Request" Type to send empty ICMP packet
 
 	}
 
-	fmt.Println("Sent encoded string:", stringToEncode)
+	// STOP PACKET
+	time.Sleep(1 * time.Second)
+	icmpListener.WriteTo(packetBinaryEncoding, &net.IPAddr{IP: net.ParseIP(targetIP)}) // need to add specific byte number
 
+	fmt.Println("Sent encoded string")
 }
